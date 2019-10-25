677ddaeaae54e8b792a1ce064565f0466c94b35b
Sergey <Sergey.popov.fly@gmail.com>
2019-10-26 00:37:30 +0300

    I don't know what i do

--- app/src/main/java/com/psb/bitsandpizzas/MainActivity.kt
+++ app/src/main/java/com/psb/bitsandpizzas/MainActivity.kt
@@ -2,11 +2,15 @@ package com.psb.bitsandpizzas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        jul.setOnClickListener {
+            tb.setText("Я люблю ЮЛЮ")
+        }
     }
 }
