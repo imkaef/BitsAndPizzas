f2f7aa6883c97841a6ca414c7499cedfaf9cde9c
Sergey <Sergey.popov.fly@gmail.com>
2019-10-26 13:39:29 +0300

    Add good appbar

--- app/src/main/java/com/psb/bitsandpizzas/MainActivity.kt
+++ app/src/main/java/com/psb/bitsandpizzas/MainActivity.kt
@@ -2,6 +2,8 @@ package com.psb.bitsandpizzas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolBar))
         jul.setOnClickListener {
             tb.setText("Я люблю ЮЛЮ")
         }
