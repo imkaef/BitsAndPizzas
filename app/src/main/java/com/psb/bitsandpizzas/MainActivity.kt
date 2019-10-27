63b1811930daf0d153dbe7bb77ab7b2a53a4def5
Sergey <Sergey.popov.fly@gmail.com>
2019-10-28 02:03:07 +0300

    Set Menu and add fingerprint button

--- app/src/main/java/com/psb/bitsandpizzas/MainActivity.kt
+++ app/src/main/java/com/psb/bitsandpizzas/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.psb.bitsandpizzas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +16,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolBar))
         jul.setOnClickListener {
-            tb.setText("Я люблю ЮЛЮ")
+            val int12 = Intent(this,OrderActivity::class.java)
+            startActivity(int12)
         }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+            when (item.itemId){
+                R.id.action_create_order -> {
+                    val intent = Intent(this, OrderActivity::class.java)
+                    startActivity(intent)
+                }
+                else -> return super.onOptionsItemSelected(item)
+            }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/psb/bitsandpizzas/OrderActivity.kt
@@ -0,0 +1,15 @@
+package com.psb.bitsandpizzas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_order.*
+
+class OrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+        setSupportActionBar(findViewById(R.id.toolBar))
+
+    }
+}
